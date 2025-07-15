57db95747ef793a0b926521beb0a9eff575e804f
IbyGit <italobbn@hotmail.com>
2025-07-15 19:28:59 -0300

    a

--- ProjU3.py
+++ ProjU3.py
@@ -89,7 +89,6 @@ var options = {
 }
 """
 marvel_net.set_options(custom_options)
-marvel_net.show_buttons(filter_=['physics'])
 
 # Save to temp file and render in Streamlit
 with tempfile.NamedTemporaryFile(delete=False, suffix=".html") as tmp_file:
